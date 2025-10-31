6e677e8c30c7149914de813caf8a94c59b9f064f
Philv2dot1 <philv2dot1@gmail.com>
2025-10-30 22:30:58 -0400

    Simplify config and serve manifest as static file again

--- pages/api/farcaster-manifest.ts
+++
@@ -1,5 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.redirect(307, 'https://api.farcaster.xyz/miniapps/hosted-manifest/0199fcb9-42f5-56f0-d6fd-9c5a7fca8daf');
-}
